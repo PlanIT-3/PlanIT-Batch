109cba843e09d75fd41cab060a71fe842d186abd
bronze_yun_4521 <ehddbs452100@naver.com>
2025-08-07 10:14:24 +0900

    fix: #10 중복 빈 삭제

--- src/main/java/woojooin/planitbatch/global/config/ServletConfig.java
+++ src/main/java/woojooin/planitbatch/global/config/ServletConfig.java
@@ -1,6 +1,9 @@
 package woojooin.planitbatch.global.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -9,9 +12,14 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
-@ComponentScan(basePackages = {
-	"woojooin.planitbatch"
-})
+@ComponentScan(
+	basePackages = "woojooin.planitbatch",
+	useDefaultFilters = false,
+	includeFilters = {
+		@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Controller.class),
+		@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = RestController.class)
+	}
+)
 public class ServletConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
