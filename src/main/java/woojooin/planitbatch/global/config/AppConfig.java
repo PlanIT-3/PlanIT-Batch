bf87cc7fa115fdd2e478cd464c441e7ede6529d9
greensnapback0229 <smdmim@gmail.com>
2025-08-07 10:23:57 +0900

    fix: 컴포넌트 스캔 범위 변경

--- src/main/java/woojooin/planitbatch/global/config/AppConfig.java
+++ src/main/java/woojooin/planitbatch/global/config/AppConfig.java
@@ -3,15 +3,25 @@ package woojooin.planitbatch.global.config;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableBatchProcessing
 @EnableScheduling
-@ComponentScan(basePackages = "woojooin.planitbatch")
 @PropertySource("classpath:application.properties")
 @Import({DatabaseConfig.class})
+@ComponentScan(
+	basePackages = "woojooin.planitbatch",
+	// 컨트롤러 제외 스캔
+	excludeFilters = @ComponentScan.Filter(
+		type = FilterType.ANNOTATION,
+		classes = {Controller.class, RestController.class}
+	)
+)
 public class AppConfig {
 }
