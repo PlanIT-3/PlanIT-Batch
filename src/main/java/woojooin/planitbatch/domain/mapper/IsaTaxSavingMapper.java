c1c70623b8e85a9992b1a58c657fdb65c68ee289
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-08-07 10:46:30 +0900

    refactor: 코드 정리(#11)

--- src/main/java/woojooin/planitbatch/domain/mapper/IsaTaxSavingMapper.java
+++ src/main/java/woojooin/planitbatch/domain/mapper/IsaTaxSavingMapper.java
@@ -1,12 +1,7 @@
 package woojooin.planitbatch.domain.mapper;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import woojooin.planitbatch.domain.dto.UserProductQuarterData;
 import woojooin.planitbatch.domain.vo.IsaTaxSavingHistoryVo;
@@ -15,7 +10,5 @@ public interface IsaTaxSavingMapper {
 	List<UserProductQuarterData> selectIsaProductProfitByMember();
 
 	int upsertIsaTaxSavingHistory(IsaTaxSavingHistoryVo vo);
-
-	int upsertIsaTaxSavingHistoryBatch(@Param("items") List<IsaTaxSavingHistoryVo> items);
 
 }
