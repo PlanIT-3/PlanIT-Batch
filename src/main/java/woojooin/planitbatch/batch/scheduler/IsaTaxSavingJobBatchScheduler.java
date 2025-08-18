078ab293cc2c20a11b908b710113eed80e7ad6eb
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-08-18 17:42:35 +0900

    refactor: 파일 이름 수정

--- src/main/java/woojooin/planitbatch/batch/scheduler/IsaTaxSavingJobBatchScheduler.java
+++ src/main/java/woojooin/planitbatch/batch/scheduler/IsaTaxSavingJobBatchScheduler.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class BatchScheduler {
+public class IsaTaxSavingJobBatchScheduler {
 
 	private final JobLauncher jobLauncher;
 	private final JobExplorer jobExplorer;
