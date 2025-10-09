001898cf322d8eeef003aaf68036e1d2c8e75cbe
greensnapback0229 <smdmim@gmail.com>
2025-10-10 06:04:15 +0900

    fix: 월 배치 주기 변경

--- src/main/java/woojooin/planitbatch/global/config/ServletConfig.java
+++ src/main/java/woojooin/planitbatch/global/config/ServletConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
-@ComponentScan(basePackages = {"woojooin.planitbatch.batch.schedular", "woojooin.planitbatch.global.controller"})
+@ComponentScan(basePackages = "woojooin.planitbatch")
 public class ServletConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
