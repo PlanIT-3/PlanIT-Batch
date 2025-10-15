3714e2d76f2c3647536ec3533852b867b52c9227
greensnapback0229 <smdmim@gmail.com>
2025-10-15 16:31:04 +0900

    feat: 배치 executor 설정

--- src/main/java/woojooin/planitbatch/batch/partitioner/RebalancePartitioner.java
+++ src/main/java/woojooin/planitbatch/batch/partitioner/RebalancePartitioner.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.ExecutionContext;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -15,8 +17,20 @@ import woojooin.planitbatch.domain.rebalance.repository.BalanceRepository;
 public class RebalancePartitioner implements Partitioner {
 	private final BalanceRepository balanceRepository;
 
+	@Qualifier("batchTaskExecutor")
+	private final ThreadPoolTaskExecutor batchTaskExecutor;
+
 	@Override
 	public Map<String, ExecutionContext> partition(int gridSize) {
+
+		System.out.println("=== [RebalancePartitioner] Executor Info ===");
+		System.out.println("ThreadNamePrefix : " + batchTaskExecutor.getThreadNamePrefix());
+		System.out.println("CorePoolSize     : " + batchTaskExecutor.getCorePoolSize());
+		System.out.println("MaxPoolSize      : " + batchTaskExecutor.getMaxPoolSize());
+		System.out.println(
+			"QueueCapacity    : " + batchTaskExecutor.getThreadPoolExecutor().getQueue().remainingCapacity());
+		System.out.println("===========================================");
+
 		int total = balanceRepository.countAll();
 		int targetSize = (int)Math.ceil(total / (double)gridSize);
 
