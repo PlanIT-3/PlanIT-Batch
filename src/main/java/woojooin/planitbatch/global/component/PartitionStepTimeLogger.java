cd4bdbe6a1d2423dd3d0d93e8032f570e0b7a125
greensnapback0229 <smdmim@gmail.com>
2025-10-18 08:56:44 +0900

    chore: 파티션별 처리량 로그 추가

--- src/main/java/woojooin/planitbatch/global/component/PartitionStepTimeLogger.java
+++ src/main/java/woojooin/planitbatch/global/component/PartitionStepTimeLogger.java
@@ -17,7 +17,8 @@ public class PartitionStepTimeLogger extends StepExecutionListenerSupport {
 	public void beforeStep(StepExecution stepExecution) {
 		startTime = System.currentTimeMillis();
 		log.info("[{}] Step started on thread: {}",
-			stepExecution.getStepName(), Thread.currentThread().getName());
+			stepExecution.getStepName(),
+			Thread.currentThread().getName());
 	}
 
 	@Override
@@ -25,11 +26,19 @@ public class PartitionStepTimeLogger extends StepExecutionListenerSupport {
 		long endTime = System.currentTimeMillis();
 		long elapsed = endTime - startTime;
 
-		log.info("[{}] Step finished on thread: {} | elapsed={} ms ({} sec)",
+		// ✅ 기존 실행시간 로그 + 처리 카운트 로깅 추가
+		log.info(
+			"[{}] Step finished on thread: {} | elapsed={} ms ({} sec) | readCount={} | writeCount={} | commitCount={} | skipCount={}",
 			stepExecution.getStepName(),
 			Thread.currentThread().getName(),
 			elapsed,
-			elapsed / 1000.0);
+			elapsed / 1000.0,
+			stepExecution.getReadCount(),
+			stepExecution.getWriteCount(),
+			stepExecution.getCommitCount(),
+			stepExecution.getSkipCount()
+		);
+
 		return stepExecution.getExitStatus();
 	}
 }
